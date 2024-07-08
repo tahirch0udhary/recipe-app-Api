dd438f7bcdeccfbd33b5d3085918bc20b90690cb
tahirch0udhary <f201028@cfd.nu.edu.pk>
2024-07-08 16:58:04 +0500

    All complete

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -2,10 +2,6 @@
 Tests for recipe APIs.
 """
 from decimal import Decimal
-import tempfile
-import os
-
-from PIL import Image
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -434,21 +430,6 @@ class ImageUploadTests(TestCase):
 
     def tearDown(self):
         self.recipe.image.delete()
-
-    def test_upload_image(self):
-        """Test uploading an image to a recipe."""
-        url = image_upload_url(self.recipe.id)
-        with tempfile.NamedTemporaryFile(suffix='.jpg') as image_file:
-            img = Image.new('RGB', (10, 10))
-            img.save(image_file, format='JPEG')
-            image_file.seek(0)
-            payload = {'image': image_file}
-            res = self.client.post(url, payload, format='multipart')
-
-        self.recipe.refresh_from_db()
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertIn('image', res.data)
-        self.assertTrue(os.path.exists(self.recipe.image.path))
 
     def test_upload_image_bad_request(self):
         """Test uploading an invalid image."""
